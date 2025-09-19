a370dace7b62e14b41d06a180428de213e9eebbe
cptkidd62 <zelinka15@gmail.com>
2025-09-19 19:44:10 +0200

    group results by bot name (started)

--- client/src/views/ResultsView.vue
+++ client/src/views/ResultsView.vue
@@ -3,20 +3,55 @@ import { onMounted, ref } from 'vue';
 import ResultListItem from '../components/ResultListItem.vue'
 import axios from 'axios';
 import { useAuthStore } from '@/stores/auth';
+import type { Match } from '@/types/match';
 
 const API_URL = 'http://localhost:3000/matches'
-const matches = ref([]);
+const matches = ref<Match[]>([]);
+const groupedMatches = ref<MatchesGroup[]>([]);
 const auth = useAuthStore()
+
+type MatchesGroup = {
+  botName: string;
+  matches: Match[];
+};
 
 const loadMatches = async () => {
   try {
     const response = await axios.get(auth.token ? `${API_URL}?userId=${auth.user?.id}` : API_URL);
     matches.value = response.data;
     console.log(matches);
+    groupedMatches.value = groupMatchesByMyBots(matches.value as Match[], 0);
+    console.log(groupedMatches);
   } catch (error) {
     console.error('Błąd ładowania meczy', error);
   }
 };
+
+function groupMatchesByMyBots(matches: Match[], currentUserId: number): MatchesGroup[] {
+  const map = new Map<string, Match[]>();
+
+  for (const match of matches) {
+    let botName: string | null = null;
+
+    if (match.user_id1 === currentUserId) {
+      botName = match.botname1;
+    } else if (match.user_id2 === currentUserId) {
+      botName = match.botname2;
+    }
+
+    if (botName) {
+      if (!map.has(botName)) {
+        map.set(botName, []);
+      }
+      map.get(botName)!.push(match);
+    }
+  }
+
+  return Array.from(map.entries()).map(([botName, matches]) => ({
+    botName,
+    matches,
+  }));
+}
 
 onMounted(loadMatches)
 </script>
