b806aa54c615ffc48f3a6fd32293ecbb948a2eb6
cptkidd62 <zelinka15@gmail.com>
2026-05-18 14:21:46 +0200

    remove in memory version of data repository

--- server/src/data/data.module.ts
+++ server/src/data/data.module.ts
@@ -1,13 +1,7 @@
 import { Module } from '@nestjs/common';
 import { FileRepositoryModule } from './file/file-repository.module';
-import { InMemoryRepositoryModule } from './in-memory/in-memory-repository.module';
 
-const RepositoryModule =
-  process.env.REPO_TYPE === 'file'
-    ? FileRepositoryModule
-    : process.env.REPO_TYPE === 'memory'
-      ? InMemoryRepositoryModule
-      : FileRepositoryModule;
+const RepositoryModule = FileRepositoryModule;
 
 @Module({
   imports: [RepositoryModule],
