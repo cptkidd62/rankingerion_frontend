380828b9b41425d1671b50c02bca366bf1b922c5
cptkidd62 <zelinka15@gmail.com>
2026-06-10 11:29:15 +0200

    change component html layout

--- client/src/components/BotListItem.vue
+++ client/src/components/BotListItem.vue
@@ -22,15 +22,15 @@ export default defineComponent({
 </script>
 
 <template>
-  <RouterLink :to="`/boty/${bot.id}`">
-    <div class="bot-list-item">
+  <div class="bot-list-item">
+    <RouterLink :to="`/boty/${bot.id}`">
       <h3 class="name">{{ bot.name }}</h3>
       <div v-if="bot.status.type === 'compilation_error'" class="error">Błąd kompilacji!</div>
       <div v-else-if="bot.status.type === 'playtime_error'" class="error">Błąd rozgrywki!</div>
       <div v-else>Rating: {{ bot.rating }}</div>
-      <button class="button" @click="handleDelete(bot.id)">usuń</button>
-    </div>
-  </RouterLink>
+    </RouterLink>
+    <button class="button" @click="handleDelete(bot.id)">usuń</button>
+  </div>
 </template>
 
 <style>
