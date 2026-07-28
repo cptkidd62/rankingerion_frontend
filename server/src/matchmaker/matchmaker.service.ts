21e63593fb955e9aaf9eec11f39008a23a37e0b2
cptkidd62 <zelinka15@gmail.com>
2026-07-28 22:47:52 +0200

    fix

--- server/src/matchmaker/matchmaker.service.ts
+++ server/src/matchmaker/matchmaker.service.ts
@@ -20,15 +20,10 @@ export class MatchmakerService implements OnModuleInit {
       this.id = 0;
     }
     this.bots = await this.botRepository.findAll();
-    const bot_p = this.bots[id];
-    const bot_o = this.bots[this.id];
-    console.log(this.bots);
-    console.log(id);
-    console.log(this.id);
-    console.log(bot_o);
-    console.log(bot_p);
     const i = this.id;
-    while (this.id++ < this.bots.length) {
+    const bot_p = this.bots[id];
+    while (this.id < this.bots.length) {
+      const bot_o = this.bots[this.id++];
       if (bot_p.user_id != bot_o.user_id) {
         if (bot_o.status.type == 'created' || bot_o.status.type == 'ok') {
           return i;
