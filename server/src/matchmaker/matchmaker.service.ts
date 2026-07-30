d8a4fa8e8e27b71cd95ea0b2fe28e273090bf68f
cptkidd62 <zelinka15@gmail.com>
2026-07-30 22:40:01 +0200

    make matchmaker draw score function on random and increase matches number to 100

--- server/src/matchmaker/matchmaker.service.ts
+++ server/src/matchmaker/matchmaker.service.ts
@@ -7,9 +7,7 @@ export class MatchmakerService {
   matchesPlanned: Map<number, number>;
 
   private readonly noise: number = 25;
-  private readonly M1 = 10;
-  private readonly M2 = 3;
-  private readonly M3 = 2;
+  private readonly MATCHES = 100;
 
   constructor(private readonly botRepository: BotRepository) {
     this.matchesPlanned = new Map();
@@ -44,6 +42,10 @@ export class MatchmakerService {
     this.bots = await this.botRepository.findAll();
     const matches =
       this.matchesPlanned.get(id) ?? this.bots[id].rating.matchesPlayed;
+    if (matches >= this.MATCHES) {
+      this.matchesPlanned.delete(id);
+      return null;
+    }
 
     let score: {
       (rating_opp: RatingData, rating_own: RatingData): number;
@@ -51,15 +53,16 @@ export class MatchmakerService {
       (rating_opp: RatingData, _rating_own: RatingData): number;
       (rating_opp: RatingData, rating_own: RatingData): number;
     };
-    if (matches < this.M1) {
+    const r = Math.random();
+    if (r < 0.66) {
+      console.log(r, 1);
       score = this.scoreOpponent1;
-    } else if (matches < this.M1 + this.M2) {
+    } else if (r < 0.86) {
+      console.log(r, 2);
       score = this.scoreOpponent2;
-    } else if (matches < this.M1 + this.M2 + this.M3) {
-      score = this.scoreOpponent3;
     } else {
-      this.matchesPlanned.delete(id);
-      return null;
+      console.log(r, 3);
+      score = this.scoreOpponent3;
     }
     const queue = this.generateQueue(this.bots, id, score);
     for (const id2 of queue) {
