68fecec7fdcf3fb4cef2f7833306ec0d1f56e7bf
cptkidd62 <zelinka15@gmail.com>
2026-08-13 15:27:50 +0200

    bot list item overhaul

--- client/src/components/BotListItem.vue
+++ client/src/components/BotListItem.vue
@@ -26,10 +26,16 @@ function handleDelete(id: number) {
 <template>
   <div v-if="showDeleted || bot.status.type !== 'deleted'" class="bot-list-item">
     <RouterLink :to="`/bots/${bot.id}`">
-      <h3 class="name">{{ bot.name }}{{ bot.status.progress == 'in_progress' ? ` (In progress: ${bot.rating.matchesPlayed}/${useConfigStore().config?.matchesToPlay})` : ''}} {{ bot.status.type === 'deleted' ? '(deleted)' : '' }}</h3>
-      <div v-if="bot.status.type === 'compilation_error'" class="error">Compilation error!</div>
-      <div v-else-if="bot.status.type === 'playtime_error'" class="error">Playtime error!</div>
-      <div v-else>Rating: {{ Math.round(bot.rating.value * 100) / 100 }}</div>
+      <h3 class="name">{{ bot.name }}</h3>
+      <div class="info">
+        {{ bot.status.progress == 'in_progress' ? `In progress:
+        ${bot.rating.matchesPlayed}/${useConfigStore().config?.matchesToPlay}` : '' }}{{
+          bot.status.type === 'deleted'
+            ? '(deleted)' : '' }}
+      </div>
+      <div v-if="bot.status.type === 'compilation_error'" class="error rating">Compilation error!</div>
+      <div v-else-if="bot.status.type === 'playtime_error'" class="error rating">Playtime error!</div>
+      <div v-else class="rating">Rating: {{ Math.round(bot.rating.value * 100) / 100 }}</div>
     </RouterLink>
     <button v-if="bot.status.type !== 'deleted'" class="button" @click="handleDelete(bot.id)">delete</button>
   </div>
@@ -37,17 +43,47 @@ function handleDelete(id: number) {
 
 <style scoped>
 .bot-list-item {
-  display: flex;
-  flex-flow: row;
+  display: grid;
   justify-self: center;
   margin-bottom: 1rem;
+  position: relative;
+  min-width: 250px;
+  min-height: 80px;
+  background-color: rgba(12, 141, 192, 0.1);
+}
+
+.bot-list-item>a {
+  display: block;
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
+
+.bot-list-item>button {
+  position: absolute;
+  bottom: 7px;
+  right: 7px;
+}
+
+.info {
+  margin: 0px 10px;
 }
 
 .name {
-  margin-right: 1rem;
+  margin: 2px 7px;
+  margin-bottom: 0px;
+  font-weight: bolder;
 }
 
 .error {
   color: red;
+  font-weight: bold;
+}
+
+.rating {
+  position: absolute;
+  bottom: 2px;
+  left: 10px;
 }
 </style>
