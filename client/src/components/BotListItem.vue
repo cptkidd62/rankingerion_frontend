98eb860947eb4153bba22d47c92cc718990b52e1
cptkidd62 <zelinka15@gmail.com>
2026-08-17 16:27:27 +0200

    basic my bots table

--- client/src/components/BotListItem.vue
+++ client/src/components/BotListItem.vue
@@ -2,6 +2,7 @@
 import { type PropType } from 'vue'
 import type { Bot } from '@/types/bot';
 import { useConfigStore } from '@/stores/config';
+import { useBotsStore } from '@/stores/bots';
 
 const props = defineProps({
   bot: {
@@ -21,37 +22,73 @@ const emits = defineEmits<{
 function handleDelete(id: number) {
   emits('delete', id);
 }
+
+const formatter = new Intl.DateTimeFormat('en-GB', {
+  dateStyle: 'medium',
+  timeStyle: 'short',
+});
+
+function displayStatus(bot: Bot): string {
+  if (bot.status.isDeleted) {
+    return 'deleted';
+  } else if (bot.status.type == 'compilation_error') {
+    return 'compilation error';
+  } else if (bot.status.type == 'playtime_error') {
+    return 'playtime error';
+  } else if (bot.status.progress != 'in_progress') {
+    return 'ok';
+  } else if (bot.status.progress == 'in_progress') {
+    return `in progress (${bot.rating.matchesPlayed} / ${useConfigStore().config!.matchesToPlay})`;
+  } else {
+    return '---';
+  }
+}
 </script>
 
 <template>
-  <div v-if="showDeleted || !bot.status.isDeleted" class="bot-list-item">
-    <RouterLink :to="`/bots/${bot.id}`">
-      <h3 class="name">{{ bot.name }}</h3>
-      <div class="info">
-        {{ bot.status.progress == 'in_progress' ? `In progress:
-        ${bot.rating.matchesPlayed}/${useConfigStore().config?.matchesToPlay}` : '' }}{{
-          bot.status.isDeleted
-            ? '(deleted)' : '' }}
-      </div>
-      <div v-if="bot.status.type === 'compilation_error'" class="error rating">Compilation error!</div>
-      <div v-else-if="bot.status.type === 'playtime_error'" class="error rating">Playtime error!</div>
-      <div v-else class="rating">Rating: {{ bot.rating.value.toFixed(2) }}</div>
-    </RouterLink>
-    <button v-if="!bot.status.isDeleted" class="button" @click="handleDelete(bot.id)">delete</button>
-  </div>
+  <tr v-if="showDeleted || !bot.status.isDeleted" class="bot-list-item">
+    <td>
+      <RouterLink :to="`/bots/${bot.id}`">
+        {{ bot.name }}
+      </RouterLink>
+    </td>
+    <td>
+      <RouterLink :to="`/bots/${bot.id}`">
+        {{ displayStatus(bot) }}
+      </RouterLink>
+    </td>
+    <td class="monospace">
+      <RouterLink :to="`/bots/${bot.id}`">
+        {{ useBotsStore().getRankingPosition(bot.id) > 0 ? useBotsStore().getRankingPosition(bot.id) : '---' }}
+      </RouterLink>
+    </td>
+    <td class="monospace">
+      <RouterLink :to="`/bots/${bot.id}`">
+        {{ (bot.status.type != 'compilation_error' && bot.status.type != 'playtime_error') ? bot.rating.value.toFixed(2) : '---' }}
+      </RouterLink>
+    </td>
+    <td>
+      <RouterLink :to="`/bots/${bot.id}`">
+        {{ formatter.format(new Date(bot.dateCreated)) }}
+      </RouterLink>
+    </td>
+    <td class="monospace">
+      <RouterLink :to="`/bots/${bot.id}`">
+        {{ bot.rating.matchesPlayed }}
+      </RouterLink>
+    </td>
+    <td class="monospace">
+      <RouterLink :to="`/bots/${bot.id}`">
+        {{ bot.errorsCount }}
+      </RouterLink>
+    </td>
+    <td>
+      <button v-if="!bot.status.isDeleted" class="button" @click="handleDelete(bot.id)">delete</button>
+    </td>
+  </tr>
 </template>
 
 <style scoped>
-.bot-list-item {
-  display: grid;
-  justify-self: center;
-  margin-bottom: 1rem;
-  position: relative;
-  min-width: 250px;
-  min-height: 80px;
-  background-color: rgba(12, 141, 192, 0.1);
-}
-
 .bot-list-item>a {
   display: block;
   width: 100%;
--- client/src/views/BotsView.vue
+++ client/src/views/BotsView.vue
@@ -7,6 +7,7 @@ import { useAuthStore } from '@/stores/auth';
 import { useBotsStore } from '@/stores/bots';
 import { api } from '@/api/index.ts';
 import { useConfigStore } from '@/stores/config.ts';
+import type { Bot } from '@/types/bot.ts';
 
 const auth = useAuthStore()
 const botsStore = useBotsStore()
@@ -43,6 +44,67 @@ const onDeleteBot = async (id: number) => {
   }
 }
 
+// ==== sorting table ====
+type SortColumn =
+  | 'name'
+  | 'rating.value'
+  | 'rating.matchesPlayed'
+  | 'dateCreated'
+  | 'status'
+  | 'errors';
+const sortColumn = ref<SortColumn | null>(null);
+const sortDirection = ref<'asc' | 'desc'>('asc');
+const sortedBots = computed(() => {
+  if (sortColumn.value == null) {
+    return botsStore.myBots;
+  }
+
+  return [...botsStore.myBots].sort((a, b) => {
+    const valueA = sortColumn.value == 'status' ? scoreStatus(a) : getNestedValue(a, sortColumn.value!);
+    const valueB = sortColumn.value == 'status' ? scoreStatus(b) : getNestedValue(b, sortColumn.value!);
+
+    if (valueA == null) return sortDirection.value === 'asc' ? -1 : 1;
+    if (valueB == null) return sortDirection.value === 'asc' ? 1 : -1;
+    if (valueA < valueB) return sortDirection.value === 'asc' ? -1 : 1;
+    if (valueA > valueB) return sortDirection.value === 'asc' ? 1 : -1;
+    return 0;
+  })
+});
+
+function scoreStatus(bot: Bot): number {
+  if (bot.status.isDeleted) {
+    return 10000;
+  } else if (bot.status.type == 'compilation_error') {
+    return 9000;
+  } else if (bot.status.type == 'playtime_error') {
+    return 8000;
+  } else if (bot.status.progress != 'in_progress') {
+    return 7000;
+  } else if (bot.status.progress == 'in_progress') {
+    return bot.rating.matchesPlayed;
+  } else {
+    return 0;
+  }
+}
+
+function getNestedValue(obj: unknown, path: string): unknown {
+  return path.split('.').reduce(
+    (value, key) => (value as Record<string, unknown>)?.[key],
+    obj
+  );
+}
+
+function sortBy(column: SortColumn) {
+  if (sortColumn.value === column) {
+    sortDirection.value =
+      sortDirection.value === 'asc' ? 'desc' : 'asc';
+  } else {
+    sortColumn.value = column;
+    sortDirection.value = 'asc';
+  }
+};
+// =======================
+
 function setPollingInterval() {
   if (intervalId) {
     clearInterval(intervalId);
@@ -77,8 +139,33 @@ onUnmounted(() => {
     </span>
     <div v-if="botsStore.loading && !botsStore.initialized">Loading...</div>
     <div v-else>
-      <BotListItem v-for="bot in botsStore.myBots" :key="bot.id" :bot="bot" :show-deleted="showDeleted"
-        @delete="onDeleteBot" />
+      <table>
+        <tbody>
+          <tr>
+            <th class="sorting-header" @click="sortBy('name')">Name <span v-if="sortColumn == 'name'">{{
+              sortDirection == 'asc' ? '↑' : '↓' }}</span></th>
+            <th class="sorting-header" @click="sortBy('status')">Status <span v-if="sortColumn == 'status'">{{
+              sortDirection == 'asc' ? '↑' : '↓' }}</span></th>
+            <th class="sorting-header" @click="sortBy('rating.value')">Ranking <span
+                v-if="sortColumn == 'rating.value'">{{
+                  sortDirection == 'asc' ? '↑' : '↓' }}</span></th>
+            <th class="sorting-header" @click="sortBy('rating.value')">Rating <span
+                v-if="sortColumn == 'rating.value'">{{
+                  sortDirection == 'asc' ? '↑' : '↓' }}</span></th>
+            <th class="sorting-header" @click="sortBy('dateCreated')">Created <span
+                v-if="sortColumn == 'dateCreated'">{{
+                  sortDirection == 'asc' ? '↑' : '↓' }}</span></th>
+            <th class="sorting-header" @click="sortBy('rating.matchesPlayed')">Matches <span
+                v-if="sortColumn == 'rating.matchesPlayed'">{{
+                  sortDirection == 'asc' ? '↑' : '↓' }}</span></th>
+            <th class="sorting-header" @click="sortBy('errors')">Errors <span v-if="sortColumn == 'errors'">{{
+              sortDirection == 'asc' ? '↑' : '↓' }}</span></th>
+            <th></th>
+          </tr>
+          <BotListItem v-for="bot in sortedBots" :key="bot.id" :bot="bot" :show-deleted="showDeleted"
+            @delete="onDeleteBot" />
+        </tbody>
+      </table>
       <details v-if="botscount < configStore.config!.maxBotsPerUser" :open="isOpen">
         <summary @click.prevent="isOpen = !isOpen">Add bot</summary>
         <NewBotForm @submit="onCreateBot" @input-change="errorMsg = ''" />
